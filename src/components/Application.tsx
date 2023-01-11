73b4cd061306922745a1746a5097186bc8a47314
VladIvanchuk <gidra465@gmail.com>
2023-01-11 22:49:38 +0200

    initial commit

---
+++ src/components/Application.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/Application.scss';
+
+const Application: React.FC = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+export default Application;
